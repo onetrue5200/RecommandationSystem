6522aae0b69339495c2c39f451d6d6713f70bdd7
onetrue5200 <oneruexie@gmail.com>
2022-12-19 18:09:03 +0800

    set logger

--- demo/kgat01/main.py
+++ demo/kgat01/main.py
@@ -1,6 +1,21 @@
+import random
+import numpy as np
+import torch
+
 from parser import parse_args
+from log_helper import *
+
+
+def train(args):
+    # seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+    log_save_id = create_log_id(args.save_dir)
 
 
 if __name__ == "__main__":
     args = parse_args()
-    print(args)
+    train(args)
